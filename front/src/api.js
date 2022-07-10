7eba6c6e545d5a9fcf77e64023dd7cfbeb12c878
Naayeon <naaay097@gmail.com>
2022-07-11 00:32:46 +0900

    Modify API

--- front/src/api.js
+++ front/src/api.js
@@ -6,10 +6,6 @@ const serverUrl =
 
 async function get(endpoint, params = '') {
   return axios.get(serverUrl + endpoint + params, {
-    // JWT 토큰을 헤더에 담아 백엔드 서버에 보냄.
-    headers: {
-      Authorization: `Bearer ${sessionStorage.getItem('userToken')}`,
-    },
     withCredentials: true,
   });
 }
@@ -22,21 +18,17 @@ async function post(endpoint, data) {
   return axios.post(serverUrl + endpoint, bodyData, {
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${sessionStorage.getItem('userToken')}`,
     },
     withCredentials: true,
   });
 }
 
 async function put(endpoint, data) {
-  // JSON.stringify 함수: Javascript 객체를 JSON 형태로 변환함.
-  // 예시: {name: "Kim"} => {"name": "Kim"}
   const bodyData = JSON.stringify(data);
 
   return axios.put(serverUrl + endpoint, bodyData, {
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${sessionStorage.getItem('userToken')}`,
     },
     withCredentials: true,
   });
@@ -44,9 +36,6 @@ async function put(endpoint, data) {
 
 async function del(endpoint, params = '') {
   return axios.delete(serverUrl + endpoint + params, {
-    headers: {
-      Authorization: `Bearer ${sessionStorage.getItem('userToken')}`,
-    },
     withCredentials: true,
   });
 }
