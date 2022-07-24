444ef77b8095575a679b01bb815c7d195f55cb21
Naayeon <naaay097@gmail.com>
2022-07-24 13:17:58 +0900

    First Commit

--- front/src/pages/note/EmotionList.js
+++ front/src/pages/note/EmotionList.js
@@ -29,6 +29,7 @@ const EmotionList = () => {
   const [search, setSearch] = useState('');
   const [target, setTarget] = useState(null); // target
 
+  // 
   useEffect(() => {
     let observer;
     if (target) {
